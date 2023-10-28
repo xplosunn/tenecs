9fd19fb41fa68cce50f4d818b8de4dc1ffa614f7
Hugo Sousa <gi.ciberon@gmail.com>
2023-10-28 18:25:27 +0200

    Add tenecs.string.hasPrefix

--- codegen/standard_library/tenecs_string.go
+++ codegen/standard_library/tenecs_string.go
@@ -6,3 +6,11 @@ func tenecs_string_join() Function {
 		body("return Pleft.(string) + Pright.(string)"),
 	)
 }
+
+func tenecs_string_hasPrefix() Function {
+	return function(
+		imports("strings"),
+		params("Pstr", "Pprefix"),
+		body("return strings.HasPrefix(Pstr.(string), Pprefix.(string))"),
+	)
+}
--- typer/standard_library/tenecs_string.go
+++ typer/standard_library/tenecs_string.go
@@ -4,6 +4,7 @@ import "github.com/xplosunn/tenecs/typer/types"
 
 var tenecs_string = packageWith(
 	withFunction("join", tenecs_string_join),
+	withFunction("hasPrefix", tenecs_string_hasPrefix),
 )
 
 var tenecs_string_join = &types.Function{
@@ -18,4 +19,18 @@ var tenecs_string_join = &types.Function{
 		},
 	},
 	ReturnType: types.String(),
+}
+
+var tenecs_string_hasPrefix = &types.Function{
+	Arguments: []types.FunctionArgument{
+		types.FunctionArgument{
+			Name:         "str",
+			VariableType: types.String(),
+		},
+		types.FunctionArgument{
+			Name:         "prefix",
+			VariableType: types.String(),
+		},
+	},
+	ReturnType: types.Boolean(),
 }
