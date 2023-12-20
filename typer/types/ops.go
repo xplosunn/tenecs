bd3314ff96ecaf96db4361bb8ede9ce915ced581
Hugo Sousa <hugo.sousa@scaling.ch>
2023-12-20 11:55:49 +0100

    Improve variable type equality on functions

--- typer/types/ops.go
+++ typer/types/ops.go
@@ -87,8 +87,8 @@ func VariableTypeEq(v1 VariableType, v2 VariableType) bool {
 		return v1CaseTypeArgument.Name == v2CaseTypeArgument.Name
 	}
 	if v1CaseFunction != nil && v2CaseFunction != nil {
-		f1 := *v1CaseFunction
-		f2 := *v2CaseFunction
+		f1 := normalizeGenericsFunction(*v1CaseFunction, "", map[string]string{})
+		f2 := normalizeGenericsFunction(*v2CaseFunction, "", map[string]string{})
 		if len(f1.Arguments) != len(f2.Arguments) {
 			return false
 		}
@@ -138,4 +138,67 @@ func VariableTypeEq(v1 VariableType, v2 VariableType) bool {
 		}
 	}
 	return reflect.DeepEqual(v1, v2)
+}
+
+func normalizeGenericsVariableType(varType VariableType, prefix string, normalizationMap map[string]string) VariableType {
+	caseTypeArgument, caseKnownType, caseFunction, caseOr := varType.VariableTypeCases()
+	if caseTypeArgument != nil {
+		return normalizeGenericsTypeArgument(*caseTypeArgument, prefix, normalizationMap)
+	} else if caseKnownType != nil {
+		return normalizeGenericsKnownType(*caseKnownType, prefix, normalizationMap)
+	} else if caseFunction != nil {
+		return normalizeGenericsFunction(*caseFunction, prefix, normalizationMap)
+	} else if caseOr != nil {
+		return normalizeGenericsOrVariableType(*caseOr, prefix, normalizationMap)
+	} else {
+		panic(fmt.Errorf("cases on %v", varType))
+	}
+}
+
+func normalizeGenericsTypeArgument(typeArg TypeArgument, prefix string, normalizationMap map[string]string) *TypeArgument {
+	newValue, ok := normalizationMap[typeArg.Name]
+	if ok {
+		return &TypeArgument{Name: newValue}
+	}
+	return &typeArg
+}
+
+func normalizeGenericsKnownType(knownType KnownType, prefix string, normalizationMap map[string]string) *KnownType {
+	return &knownType
+}
+
+func normalizeGenericsFunction(function Function, prefix string, previousNormalizationMap map[string]string) *Function {
+	normalizationMap := map[string]string{}
+	for k, v := range previousNormalizationMap {
+		normalizationMap[k] = v
+	}
+
+	normalized := Function{
+		Generics:   []string{},
+		Arguments:  []FunctionArgument{},
+		ReturnType: nil,
+	}
+
+	for i, generic := range function.Generics {
+		newTypeName := fmt.Sprintf("%s%d", prefix, i)
+		normalizationMap[generic] = newTypeName
+		normalized.Generics = append(normalized.Generics, newTypeName)
+	}
+	for i, functionArgument := range normalized.Arguments {
+		normalized.Arguments = append(normalized.Arguments, FunctionArgument{
+			Name:         functionArgument.Name,
+			VariableType: normalizeGenericsVariableType(functionArgument.VariableType, fmt.Sprintf("%s_%d", prefix, i), normalizationMap),
+		})
+	}
+	normalized.ReturnType = normalizeGenericsVariableType(function.ReturnType, fmt.Sprintf("%s_R", prefix), normalizationMap)
+
+	return &normalized
+}
+
+func normalizeGenericsOrVariableType(or OrVariableType, prefix string, normalizationMap map[string]string) *OrVariableType {
+	result := OrVariableType{Elements: []VariableType{}}
+	for _, element := range or.Elements {
+		VariableTypeAddToOr(normalizeGenericsVariableType(element, prefix, normalizationMap), &result)
+	}
+	return &result
 }
