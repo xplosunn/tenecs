f89e114558b3b5186e824e76caf9965580e7af58
Hugo Sousa <gi.ciberon@gmail.com>
2025-07-01 08:50:56 +0200

    Add desugar package

---
+++ desugar/api.go
@@ -0,0 +1,363 @@
+package desugar
+
+import (
+	"github.com/alecthomas/participle/v2/lexer"
+	"github.com/xplosunn/tenecs/parser"
+)
+
+type Node struct {
+	Pos    lexer.Position
+	EndPos lexer.Position
+}
+
+type FileTopLevel struct {
+	Tokens               []lexer.Token
+	Package              Package               `@@`
+	Imports              []Import              `@@*`
+	TopLevelDeclarations []TopLevelDeclaration `@@*`
+}
+
+func FileTopLevelFields(node FileTopLevel) (Package, []Import, []TopLevelDeclaration) {
+	return node.Package, node.Imports, node.TopLevelDeclarations
+}
+
+type Name struct {
+	Node
+	String string
+}
+
+type Package struct {
+	Node
+	DotSeparatedNames []Name
+}
+
+type Import struct {
+	Node
+	DotSeparatedVars []Name
+	As               *Name
+}
+
+func ImportFields(node Import) ([]Name, *Name) {
+	return node.DotSeparatedVars, node.As
+}
+
+type TopLevelDeclaration interface {
+	sealedTopLevelDeclaration()
+}
+
+func TopLevelDeclarationExhaustiveSwitch(
+	topLevelDeclaration TopLevelDeclaration,
+	caseDeclaration func(topLevelDeclaration Declaration),
+	caseStruct func(topLevelDeclaration Struct),
+	caseTypeAlias func(topLevelDeclaration TypeAlias),
+) {
+	declaration, ok := topLevelDeclaration.(Declaration)
+	if ok {
+		caseDeclaration(declaration)
+		return
+	}
+	struc, ok := topLevelDeclaration.(Struct)
+	if ok {
+		caseStruct(struc)
+		return
+	}
+	typeAlias, ok := topLevelDeclaration.(TypeAlias)
+	if ok {
+		caseTypeAlias(typeAlias)
+		return
+	}
+}
+
+type Struct struct {
+	Name      Name
+	Generics  []Name
+	Variables []StructVariable
+}
+
+func (s Struct) sealedTopLevelDeclaration() {}
+
+func StructFields(struc Struct) (Name, []Name, []StructVariable) {
+	return struc.Name, struc.Generics, struc.Variables
+}
+
+type StructVariable struct {
+	Name Name
+	Type TypeAnnotation
+}
+
+func StructVariableFields(structVariable StructVariable) (Name, TypeAnnotation) {
+	return structVariable.Name, structVariable.Type
+}
+
+type TypeAlias struct {
+	Name     Name
+	Generics []Name
+	Type     TypeAnnotation
+}
+
+func (i TypeAlias) sealedTopLevelDeclaration() {}
+
+func TypeAliasFields(typeAlias TypeAlias) (Name, []Name, TypeAnnotation) {
+	return typeAlias.Name, typeAlias.Generics, typeAlias.Type
+}
+
+type TypeAnnotation struct {
+	Node
+	OrTypes []TypeAnnotationElement `@@ ("|" @@)*`
+}
+
+type TypeAnnotationElement interface {
+	sealedTypeAnnotationElement()
+}
+
+func TypeAnnotationElementExhaustiveSwitch(
+	typeAnnotationElement TypeAnnotationElement,
+	caseUnderscore func(underscoreTypeAnnotation SingleNameType),
+	caseSingleNameType func(typeAnnotation SingleNameType),
+	caseFunctionType func(typeAnnotation FunctionType),
+) {
+	singleNameType, ok := typeAnnotationElement.(SingleNameType)
+	if ok {
+		if singleNameType.TypeName.String == "_" {
+			caseUnderscore(singleNameType)
+			return
+		}
+		caseSingleNameType(singleNameType)
+		return
+	}
+	functionType, ok := typeAnnotationElement.(FunctionType)
+	if ok {
+		caseFunctionType(functionType)
+		return
+	}
+}
+
+type SingleNameType struct {
+	Node
+	TypeName Name
+	Generics []TypeAnnotation
+}
+
+func (s SingleNameType) sealedTypeAnnotationElement() {}
+
+type FunctionTypeArgument struct {
+	Name *Name
+	Type TypeAnnotation
+}
+
+type FunctionType struct {
+	Generics   []Name
+	Arguments  []FunctionTypeArgument
+	ReturnType TypeAnnotation
+}
+
+func (f FunctionType) sealedTypeAnnotationElement() {}
+
+type ArgumentsList struct {
+	Node
+	Generics  []TypeAnnotation
+	Arguments []NamedArgument
+}
+
+type NamedArgument struct {
+	Node
+	Name     *Name
+	Argument ExpressionBox
+}
+
+func NamedArgumentFields(namedArgument NamedArgument) (*Name, ExpressionBox) {
+	return namedArgument.Name, namedArgument.Argument
+}
+
+type DotOrArrowName struct {
+	Node
+	Dot     bool
+	Arrow   bool
+	VarName Name
+}
+
+type AccessOrInvocation struct {
+	Node
+	DotOrArrowName *DotOrArrowName
+	Arguments      *ArgumentsList
+}
+
+type ExpressionBox struct {
+	Node
+	Expression              Expression
+	AccessOrInvocationChain []AccessOrInvocation
+}
+
+func ExpressionBoxFields(expressionBox ExpressionBox) (Expression, []AccessOrInvocation) {
+	return expressionBox.Expression, expressionBox.AccessOrInvocationChain
+}
+
+type Expression interface {
+	sealedExpression()
+}
+
+func ExpressionExhaustiveSwitch(
+	expression Expression,
+	caseLiteralExpression func(expression LiteralExpression),
+	caseReferenceOrInvocation func(expression ReferenceOrInvocation),
+	caseLambda func(generics *LambdaOrListGenerics, expression Lambda),
+	caseDeclaration func(expression Declaration),
+	caseIf func(expression If),
+	caseList func(generics *LambdaOrListGenerics, expression List),
+	caseWhen func(expression When),
+) {
+	literalExpression, ok := expression.(LiteralExpression)
+	if ok {
+		caseLiteralExpression(literalExpression)
+		return
+	}
+	referenceOrInvocation, ok := expression.(ReferenceOrInvocation)
+	if ok {
+		caseReferenceOrInvocation(referenceOrInvocation)
+		return
+	}
+	lambdaOrList, ok := expression.(LambdaOrList)
+	if ok {
+		if lambdaOrList.List != nil {
+			caseList(lambdaOrList.Generics, *lambdaOrList.List)
+		} else {
+			caseLambda(lambdaOrList.Generics, *lambdaOrList.Lambda)
+		}
+
+		return
+	}
+	declaration, ok := expression.(Declaration)
+	if ok {
+		caseDeclaration(declaration)
+		return
+	}
+	ifExp, ok := expression.(If)
+	if ok {
+		caseIf(ifExp)
+		return
+	}
+	when, ok := expression.(When)
+	if ok {
+		caseWhen(when)
+		return
+	}
+}
+
+type When struct {
+	Node
+	Over  ExpressionBox
+	Is    []WhenIs
+	Other *WhenOther
+}
+
+func (w When) sealedExpression() {}
+
+type WhenIs struct {
+	Node
+	Name      *Name
+	Type      TypeAnnotation
+	ThenBlock []ExpressionBox
+}
+
+type WhenOther struct {
+	Node
+	Name      *Name
+	ThenBlock []ExpressionBox
+}
+
+type If struct {
+	Node
+	Condition ExpressionBox
+	ThenBlock []ExpressionBox
+	ElseIfs   []IfThen
+	ElseBlock []ExpressionBox
+}
+
+type IfThen struct {
+	Node
+	Condition ExpressionBox
+	ThenBlock []ExpressionBox
+}
+
+func (i If) sealedExpression() {}
+
+func IfFields(parserIf If) (ExpressionBox, []ExpressionBox, []IfThen, []ExpressionBox) {
+	return parserIf.Condition, parserIf.ThenBlock, parserIf.ElseIfs, parserIf.ElseBlock
+}
+
+type DeclarationShortCircuit struct {
+	TypeAnnotation *TypeAnnotation
+}
+
+type Declaration struct {
+	Name           Name
+	TypeAnnotation *TypeAnnotation
+	ShortCircuit   *DeclarationShortCircuit
+	ExpressionBox  ExpressionBox
+}
+
+func (d Declaration) sealedTopLevelDeclaration() {}
+
+func (d Declaration) sealedExpression() {}
+
+func DeclarationFields(node Declaration) (Name, *TypeAnnotation, *DeclarationShortCircuit, ExpressionBox) {
+	return node.Name, node.TypeAnnotation, node.ShortCircuit, node.ExpressionBox
+}
+
+type LiteralExpression struct {
+	Node
+	Literal parser.Literal
+}
+
+func (l LiteralExpression) sealedExpression() {}
+
+type LambdaOrListGenerics struct {
+	Node
+	Generics []TypeAnnotation
+}
+
+type LambdaOrList struct {
+	Node
+	Generics *LambdaOrListGenerics
+	List     *List
+	Lambda   *Lambda
+}
+
+func (l LambdaOrList) sealedExpression() {}
+
+type List struct {
+	Node
+	Expressions []ExpressionBox
+}
+
+type LambdaSignature struct {
+	Node
+	Parameters []Parameter
+	ReturnType *TypeAnnotation
+}
+
+type Lambda struct {
+	Node
+	Signature LambdaSignature
+	Block     []ExpressionBox
+}
+
+type Parameter struct {
+	Name Name
+	Type *TypeAnnotation
+}
+
+func ParameterFields(node Parameter) (Name, *TypeAnnotation) {
+	return node.Name, node.Type
+}
+
+type ReferenceOrInvocation struct {
+	Var       Name
+	Arguments *ArgumentsList
+}
+
+func (r ReferenceOrInvocation) sealedExpression() {}
+
+func ReferenceOrInvocationFields(node ReferenceOrInvocation) (Name, *ArgumentsList) {
+	return node.Var, node.Arguments
+}
---
+++ desugar/desugar.go
@@ -0,0 +1,325 @@
+package desugar
+
+import (
+	"github.com/xplosunn/tenecs/parser"
+)
+
+func Desugar(parsed parser.FileTopLevel) FileTopLevel {
+	imports := []Import{}
+	for _, parsed := range parsed.Imports {
+		imports = append(imports, desugarImport(parsed))
+	}
+
+	topLevelDeclarations := []TopLevelDeclaration{}
+	for _, parsed := range parsed.TopLevelDeclarations {
+		topLevelDeclarations = append(topLevelDeclarations, desugarTopLevelDeclaration(parsed))
+	}
+
+	return FileTopLevel{
+		Tokens:               parsed.Tokens,
+		Package:              desugarPackage(parsed.Package),
+		Imports:              imports,
+		TopLevelDeclarations: topLevelDeclarations,
+	}
+}
+
+func desugarSlice[In any, Out any](in []In, desugar func(In) Out) []Out {
+	result := []Out{}
+	for _, in := range in {
+		result = append(result, desugar(in))
+	}
+	return result
+}
+
+func desugarWhenNonNil[In any, Out any](ptr *In, desugar func(In) Out) *Out {
+	if ptr == nil {
+		return nil
+	}
+	result := desugar(*ptr)
+	return &result
+}
+
+func desugarPackage(parsed parser.Package) Package {
+	return Package{
+		Node:              desugarNode(parsed.Node),
+		DotSeparatedNames: desugarSlice(parsed.DotSeparatedNames, desugarName),
+	}
+}
+
+func desugarName(parsed parser.Name) Name {
+	return Name{
+		Node:   desugarNode(parsed.Node),
+		String: parsed.String,
+	}
+}
+
+func desugarNode(parsed parser.Node) Node {
+	return Node{
+		Pos:    parsed.Pos,
+		EndPos: parsed.EndPos,
+	}
+}
+
+func desugarImport(parsed parser.Import) Import {
+	return Import{
+		Node:             desugarNode(parsed.Node),
+		DotSeparatedVars: desugarSlice(parsed.DotSeparatedVars, desugarName),
+		As:               desugarWhenNonNil(parsed.As, desugarName),
+	}
+}
+
+func desugarTopLevelDeclaration(parsed parser.TopLevelDeclaration) TopLevelDeclaration {
+	var result TopLevelDeclaration
+	parser.TopLevelDeclarationExhaustiveSwitch(
+		parsed,
+		func(parsed parser.Declaration) {
+			result = desugarDeclaration(parsed)
+		},
+		func(parsed parser.Struct) {
+			result = desugarStruct(parsed)
+		},
+		func(parsed parser.TypeAlias) {
+			result = desugarTypeAlias(parsed)
+		},
+	)
+	return result
+}
+
+func desugarDeclaration(parsed parser.Declaration) Declaration {
+	return Declaration{
+		Name:           desugarName(parsed.Name),
+		TypeAnnotation: desugarWhenNonNil(parsed.TypeAnnotation, desugarTypeAnnotation),
+		ShortCircuit:   desugarWhenNonNil(parsed.ShortCircuit, desugarDeclarationShortCircuit),
+		ExpressionBox:  desugarExpressionBox(parsed.ExpressionBox),
+	}
+}
+
+func desugarExpressionBox(parsed parser.ExpressionBox) ExpressionBox {
+	return ExpressionBox{
+		Node:                    desugarNode(parsed.Node),
+		Expression:              desugarExpression(parsed.Expression),
+		AccessOrInvocationChain: desugarSlice(parsed.AccessOrInvocationChain, desugarAccessOrInvocation),
+	}
+}
+
+func desugarAccessOrInvocation(parsed parser.AccessOrInvocation) AccessOrInvocation {
+	return AccessOrInvocation{
+		Node:           desugarNode(parsed.Node),
+		DotOrArrowName: desugarWhenNonNil(parsed.DotOrArrowName, desugarDotOrArrowName),
+		Arguments:      desugarWhenNonNil(parsed.Arguments, desugarArgumentsList),
+	}
+}
+
+func desugarArgumentsList(parsed parser.ArgumentsList) ArgumentsList {
+	return ArgumentsList{
+		Node:      desugarNode(parsed.Node),
+		Generics:  desugarSlice(parsed.Generics, desugarTypeAnnotation),
+		Arguments: desugarSlice(parsed.Arguments, desugarNamedArgument),
+	}
+}
+
+func desugarNamedArgument(parsed parser.NamedArgument) NamedArgument {
+	return NamedArgument{
+		Node:     desugarNode(parsed.Node),
+		Name:     desugarWhenNonNil(parsed.Name, desugarName),
+		Argument: desugarExpressionBox(parsed.Argument),
+	}
+}
+
+func desugarDotOrArrowName(parsed parser.DotOrArrowName) DotOrArrowName {
+	return DotOrArrowName{
+		Node:    desugarNode(parsed.Node),
+		Dot:     parsed.Dot,
+		Arrow:   parsed.Arrow,
+		VarName: desugarName(parsed.VarName),
+	}
+}
+
+func desugarExpression(parsed parser.Expression) Expression {
+	var result Expression
+	parser.ExpressionExhaustiveSwitch(
+		parsed,
+		func(parsed parser.LiteralExpression) {
+			result = LiteralExpression{
+				Node:    desugarNode(parsed.Node),
+				Literal: parsed.Literal,
+			}
+		},
+		func(parsed parser.ReferenceOrInvocation) {
+			result = ReferenceOrInvocation{
+				Var:       desugarName(parsed.Var),
+				Arguments: desugarWhenNonNil(parsed.Arguments, desugarArgumentsList),
+			}
+		},
+		func(generics *parser.LambdaOrListGenerics, parsed parser.Lambda) {
+			lambda := Lambda{
+				Node:      desugarNode(parsed.Node),
+				Signature: desugarLambdaSignature(parsed.Signature),
+				Block:     desugarSlice(parsed.Block, desugarExpressionBox),
+			}
+			result = LambdaOrList{
+				Node:     desugarNode(generics.Node),
+				Generics: desugarWhenNonNil(generics, desugarLambdaOrListGenerics),
+				List:     nil,
+				Lambda:   &lambda,
+			}
+		},
+		func(parsed parser.Declaration) {
+			result = Declaration{
+				Name:           desugarName(parsed.Name),
+				TypeAnnotation: desugarWhenNonNil(parsed.TypeAnnotation, desugarTypeAnnotation),
+				ShortCircuit:   desugarWhenNonNil(parsed.ShortCircuit, desugarDeclarationShortCircuit),
+				ExpressionBox:  desugarExpressionBox(parsed.ExpressionBox),
+			}
+		},
+		func(parsed parser.If) {
+			result = If{
+				Node:      desugarNode(parsed.Node),
+				Condition: desugarExpressionBox(parsed.Condition),
+				ThenBlock: desugarSlice(parsed.ThenBlock, desugarExpressionBox),
+				ElseIfs:   desugarSlice(parsed.ElseIfs, desugarIfThen),
+				ElseBlock: desugarSlice(parsed.ElseBlock, desugarExpressionBox),
+			}
+		},
+		func(generics *parser.LambdaOrListGenerics, parsed parser.List) {
+			list := List{
+				Node:        desugarNode(parsed.Node),
+				Expressions: desugarSlice(parsed.Expressions, desugarExpressionBox),
+			}
+			result = LambdaOrList{
+				Node:     desugarNode(generics.Node),
+				Generics: desugarWhenNonNil(generics, desugarLambdaOrListGenerics),
+				List:     &list,
+				Lambda:   nil,
+			}
+		},
+		func(parsed parser.When) {
+			result = When{
+				Node:  desugarNode(parsed.Node),
+				Over:  desugarExpressionBox(parsed.Over),
+				Is:    desugarSlice(parsed.Is, desugarWhenIs),
+				Other: desugarWhenNonNil(parsed.Other, desugarWhenOther),
+			}
+		},
+	)
+	return result
+}
+
+func desugarWhenOther(parsed parser.WhenOther) WhenOther {
+	return WhenOther{
+		Node:      desugarNode(parsed.Node),
+		Name:      desugarWhenNonNil(parsed.Name, desugarName),
+		ThenBlock: desugarSlice(parsed.ThenBlock, desugarExpressionBox),
+	}
+}
+
+func desugarWhenIs(parsed parser.WhenIs) WhenIs {
+	return WhenIs{
+		Node:      desugarNode(parsed.Node),
+		Name:      desugarWhenNonNil(parsed.Name, desugarName),
+		Type:      desugarTypeAnnotation(parsed.Type),
+		ThenBlock: desugarSlice(parsed.ThenBlock, desugarExpressionBox),
+	}
+}
+
+func desugarIfThen(parsed parser.IfThen) IfThen {
+	return IfThen{
+		Node:      desugarNode(parsed.Node),
+		Condition: desugarExpressionBox(parsed.Condition),
+		ThenBlock: desugarSlice(parsed.ThenBlock, desugarExpressionBox),
+	}
+}
+
+func desugarLambdaSignature(parsed parser.LambdaSignature) LambdaSignature {
+	return LambdaSignature{
+		Node:       desugarNode(parsed.Node),
+		Parameters: desugarSlice(parsed.Parameters, desugarParameter),
+		ReturnType: desugarWhenNonNil(parsed.ReturnType, desugarTypeAnnotation),
+	}
+}
+
+func desugarParameter(parsed parser.Parameter) Parameter {
+	return Parameter{
+		Name: desugarName(parsed.Name),
+		Type: desugarWhenNonNil(parsed.Type, desugarTypeAnnotation),
+	}
+}
+
+func desugarLambdaOrListGenerics(parsed parser.LambdaOrListGenerics) LambdaOrListGenerics {
+	return LambdaOrListGenerics{
+		Node:     desugarNode(parsed.Node),
+		Generics: desugarSlice(parsed.Generics, desugarTypeAnnotation),
+	}
+}
+
+func desugarDeclarationShortCircuit(parsed parser.DeclarationShortCircuit) DeclarationShortCircuit {
+	return DeclarationShortCircuit{
+		TypeAnnotation: desugarWhenNonNil(parsed.TypeAnnotation, desugarTypeAnnotation),
+	}
+}
+
+func desugarTypeAnnotation(parsed parser.TypeAnnotation) TypeAnnotation {
+	return TypeAnnotation{
+		Node:    desugarNode(parsed.Node),
+		OrTypes: desugarSlice(parsed.OrTypes, desugarTypeAnnotationElement),
+	}
+}
+
+func desugarTypeAnnotationElement(parsed parser.TypeAnnotationElement) TypeAnnotationElement {
+	var result TypeAnnotationElement
+	parser.TypeAnnotationElementExhaustiveSwitch(
+		parsed,
+		func(parsed parser.SingleNameType) {
+			result = SingleNameType{
+				Node:     desugarNode(parsed.Node),
+				TypeName: desugarName(parsed.TypeName),
+				Generics: desugarSlice(parsed.Generics, desugarTypeAnnotation),
+			}
+		},
+		func(parsed parser.SingleNameType) {
+			result = SingleNameType{
+				Node:     desugarNode(parsed.Node),
+				TypeName: desugarName(parsed.TypeName),
+				Generics: desugarSlice(parsed.Generics, desugarTypeAnnotation),
+			}
+		},
+		func(parsed parser.FunctionType) {
+			result = FunctionType{
+				Generics:   desugarSlice(parsed.Generics, desugarName),
+				Arguments:  desugarSlice(parsed.Arguments, desugarFunctionTypeArgument),
+				ReturnType: desugarTypeAnnotation(parsed.ReturnType),
+			}
+		},
+	)
+	return result
+}
+
+func desugarFunctionTypeArgument(parsed parser.FunctionTypeArgument) FunctionTypeArgument {
+	return FunctionTypeArgument{
+		Name: desugarWhenNonNil(parsed.Name, desugarName),
+		Type: desugarTypeAnnotation(parsed.Type),
+	}
+}
+
+func desugarStruct(parsed parser.Struct) Struct {
+	return Struct{
+		Name:      desugarName(parsed.Name),
+		Generics:  desugarSlice(parsed.Generics, desugarName),
+		Variables: desugarSlice(parsed.Variables, desugarStructVariable),
+	}
+}
+
+func desugarStructVariable(parsed parser.StructVariable) StructVariable {
+	return StructVariable{
+		Name: desugarName(parsed.Name),
+		Type: desugarTypeAnnotation(parsed.Type),
+	}
+}
+
+func desugarTypeAlias(parsed parser.TypeAlias) TypeAlias {
+	return TypeAlias{
+		Name:     desugarName(parsed.Name),
+		Generics: desugarSlice(parsed.Generics, desugarName),
+		Type:     desugarTypeAnnotation(parsed.Type),
+	}
+}
